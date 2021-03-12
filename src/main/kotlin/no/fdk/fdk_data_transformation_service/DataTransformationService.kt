2e298a65c3a101d65a2cbe3f32a3fd0c0a6e37e2
NilsOveTen <github.nils.ove@tendenes.com>
2021-03-12 13:55:34 +0100

    feat: HelloWorld function

---
+++ src/main/kotlin/no/fdk/fdk_data_transformation_service/DataTransformationService.kt
@@ -0,0 +1,14 @@
+package no.fdk.fdk_data_transformation_service
+
+import com.google.cloud.functions.HttpFunction
+import com.google.cloud.functions.HttpRequest
+import com.google.cloud.functions.HttpResponse
+
+class DataTransformationService : HttpFunction {
+
+    override fun service(request: HttpRequest, response: HttpResponse) {
+        val writer = response.writer
+        writer.write("hello function")
+    }
+
+}
