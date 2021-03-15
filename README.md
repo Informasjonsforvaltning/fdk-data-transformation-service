3e3afc8befc01b13efd3a542d99c7f1c5fd60101
NilsOveTen <github.nils.ove@tendenes.com>
2021-03-15 07:43:55 +0100

    feat: add publishers endpoint

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # fdk-data-transformation-service
 A service (implemented as function) that transforms our harvested data to make them more suited for doing analytics
+
+## Requirements
+- maven
+- java 15
+- docker
+- docker-compose
+
+## Run locally
+```
+mvn function:run
+```
+
+## Headers
+Content-Type is required and should include charset, i.e: text/turtle;charset=UTF-8
+
+Accept defaults to text/turtle and will always use charset=UTF-8
--- src/main/kotlin/no/fdk/fdk_data_transformation_service/DataTransformationService.kt
+++ src/main/kotlin/no/fdk/fdk_data_transformation_service/DataTransformationService.kt
@@ -3,12 +3,40 @@ package no.fdk.fdk_data_transformation_service
 import com.google.cloud.functions.HttpFunction
 import com.google.cloud.functions.HttpRequest
 import com.google.cloud.functions.HttpResponse
+import no.fdk.fdk_data_transformation_service.rdf.createRDFResponse
+import no.fdk.fdk_data_transformation_service.rdf.headerFromJenaLang
+import no.fdk.fdk_data_transformation_service.rdf.jenaLangFromHeader
+import no.fdk.fdk_data_transformation_service.rdf.parseRDF
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.riot.Lang
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.net.HttpURLConnection
+
+val LOGGER: Logger = LoggerFactory.getLogger(DataTransformationService::class.java)
+val ENDPOINTS = listOf("/transform")
 
 class DataTransformationService : HttpFunction {
 
     override fun service(request: HttpRequest, response: HttpResponse) {
-        val writer = response.writer
-        writer.write("hello function test")
+        val contentLang = jenaLangFromHeader(request.headers["Content-Type"]?.firstOrNull())
+        val acceptLang = jenaLangFromHeader(request.headers["Accept"]?.firstOrNull())
+        when {
+            !ENDPOINTS.contains(request.path) -> response.setStatusCode(HttpURLConnection.HTTP_NOT_FOUND)
+            request.method != "POST" -> response.setStatusCode(HttpURLConnection.HTTP_BAD_METHOD)
+            contentLang == null -> response.setStatusCode(HttpURLConnection.HTTP_UNSUPPORTED_TYPE)
+            contentLang == Lang.RDFNULL -> response.setStatusCode(HttpURLConnection.HTTP_UNSUPPORTED_TYPE)
+            acceptLang == Lang.RDFNULL -> response.setStatusCode(HttpURLConnection.HTTP_NOT_ACCEPTABLE)
+            else -> parseRDF(request.reader.readText(), contentLang)
+                ?.run { enrichPublishersData(acceptLang, response) }
+                ?: run { response.setStatusCode(HttpURLConnection.HTTP_BAD_REQUEST) }
+        }
     }
+}
 
+private fun Model.enrichPublishersData(acceptLang: Lang?, response: HttpResponse) {
+    val contentLang = acceptLang ?: Lang.TURTLE
+    response.setContentType(headerFromJenaLang(contentLang))
+
+    response.writer.write(createRDFResponse(contentLang))
 }
